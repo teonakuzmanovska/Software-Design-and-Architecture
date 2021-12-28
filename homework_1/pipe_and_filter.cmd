735a090563eeea8658e6f062eeb15aa7613e0ac3
teonakuzmanovska <94232533+teonakuzmanovska@users.noreply.github.com>
2021-12-28 23:50:52 +0100

    Update pipe_and_filter.cmd

--- homework_1/pipe_and_filter.cmd
+++ homework_1/pipe_and_filter.cmd
@@ -1,5 +1,3 @@
 osmfilter skopje.osm --keep="amenity=library" | osmconvert - --all-to-nodes --csv="name addr:city addr:street opening_hours phone @lat @lon" --csv-headline --csv-separator="," -o="skopje_libraries.csv"
 
-osmfilter skopje.osm --keep="amenity=university" | osmconvert - --all-to-nodes --csv="name addr:city addr:street opening_hours phone @lat @lon" --csv-headline --csv-separator="," -o="skopje_universities.csv"
-
 osmfilter skopje.osm --keep="amenity=school" | osmconvert - --all-to-nodes --csv="name addr:city addr:street opening_hours phone @lat @lon" --csv-headline --csv-separator="," -o="skopje_schools.csv"
