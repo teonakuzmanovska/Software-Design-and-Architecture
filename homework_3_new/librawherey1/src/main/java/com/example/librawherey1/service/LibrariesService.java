51c44747aa16e6393eca9cedba0b727e9df3887f
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2022-01-30 16:50:25 +0100

    fixed links with additional controllers

---
+++ homework_3_new/librawherey1/src/main/java/com/example/librawherey1/service/LibrariesService.java
@@ -0,0 +1,11 @@
+package com.example.librawherey1.service;
+
+import com.example.librawherey1.model.Libraries;
+
+import java.util.List;
+
+public interface LibrariesService {
+//    List<Libraries> findAll();
+    List<Libraries> passNearbyLibraries();
+
+}
---
+++ homework_3_new/librawherey1/src/main/java/com/example/librawherey1/service/SchoolsService.java
@@ -0,0 +1,10 @@
+package com.example.librawherey1.service;
+
+import com.example.librawherey1.model.Schools;
+
+import java.util.List;
+
+public interface SchoolsService {
+    List<Schools> findAll();
+
+}
