d57b7fc502ed7dea0889bb8d65cfbf9d7ac234c3
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2022-01-31 23:48:21 +0100

    correcting error

--- homework_3_and_4/librawherey1/src/main/java/com/example/librawherey1/service/SchoolsService.java
+++
@@ -1,9 +0,0 @@
-package com.example.librawherey1.service;
-
-import com.example.librawherey1.model.Schools;
-
-import java.util.ArrayList;
-
-public interface SchoolsService {
-    ArrayList<Schools> findAll();
-}
